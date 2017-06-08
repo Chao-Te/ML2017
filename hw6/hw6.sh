e82418caa79c49f165a8f86bf49d0f289edeb1cf
Chao-Te Chou <cct1220@cml21.csie.ntu.edu.tw>
2017-06-08 16:53:02 +0800

    modified:   hw6.sh 	modified:   hw6_best.sh

--- hw6/hw6.sh
+++ hw6/hw6.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python3 r_test.py $1 $2
+python3 r_test.py $1 $2
 
--- hw6/hw6_best.sh
+++ hw6/hw6_best.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python3 test.py $1 $2
+python3 test.py $1 $2
 
